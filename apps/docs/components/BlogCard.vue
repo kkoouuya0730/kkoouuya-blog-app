6422156ea8afe3ec0e7d3cd3fbfc7378af00fdb0
康也　岩渕 <kouya.0730@icloud.com>
2024-01-25 20:44:10 +0900

    ブログ一覧のたたき台作成

---
+++ apps/docs/components/BlogCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { BlogType } from "@/types/blog";
+
+type Props = {
+  BlogInfo: BlogType;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <article>
+    <UCard>
+      <template #header>
+        <NuxtLink :to="BlogInfo.slag">
+          <NuxtImg format="webp" :src="BlogInfo.thumnail" />
+        </NuxtLink>
+      </template>
+
+      <p class="text-lg font-bold">
+        {{ BlogInfo.title }}
+      </p>
+      <p class="text-sm text-gray-500">
+        {{ BlogInfo.publishedAt }}
+      </p>
+      <p>
+        {{ BlogInfo.summary }}
+      </p>
+
+      <template #footer class="h-6">
+        <ul
+          class="flex flex-wrap space-x-1 gap-1"
+          v-for="tagBadgeInfo in BlogInfo.tag"
+        >
+          <TagBadge :-badge-info="tagBadgeInfo" />
+        </ul>
+      </template>
+    </UCard>
+  </article>
+</template>
--- apps/docs/pages/blog.vue
+++ apps/docs/pages/blog.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+// TODO
+const blogs = [
+  {
+    title: "2023 年に読んでよかった本",
+    slag: "/blog/1",
+    thumnail: "/ozouni_shouyu_15837.webp",
+    publishedAt: "2023.12.12", // Dateにする
+    summary:
+      "年末なので 2023 年のまとめっぽい記事を書きたくなりました。今年は 1 年間でおおよそ 300 冊の本をよんだようです（そのうち 3 割ほどはラノベなのですが...）。その中でも特に印象に残った本を紹介します。",
+    tag: [
+      {
+        label: "React",
+        color: "primary",
+      },
+    ],
+  },
+];
+</script>
+
 <template>
   <div>
-    <h1>Welcome to the blogpage</h1>
+    <ul class="grid grid-cols-3 gap-4" v-for="blogInfo in blogs">
+      <BlogCard :-blog-info="blogInfo" />
+    </ul>
   </div>
 </template>
