e66ae9725e9a65bb5da938f55401285e5deb78e5
康也　岩渕 <kouya.0730@icloud.com>
2024-01-28 11:00:58 +0900

    タグ画面の草案作成

--- apps/docs/components/TagBadge/TagBadge.vue
+++ apps/docs/components/TagBadge/TagBadge.vue
@@ -9,7 +9,19 @@ defineProps<Props>();
 </script>
 
 <template>
-  <UBadge :color="BadgeInfo.color" :ui="{ rounded: 'rounded-full' }">
+  <UBadge
+    class="hover:underline"
+    :color="BadgeInfo.color"
+    :ui="{ rounded: 'rounded-full' }"
+  >
     {{ BadgeInfo.label }}
+    <UBadge
+      v-if="BadgeInfo.isList"
+      class="ml-2"
+      color="amber"
+      variant="soft"
+      :ui="{ rounded: 'rounded-full' }"
+      >{{ BadgeInfo.amount }}</UBadge
+    >
   </UBadge>
 </template>
